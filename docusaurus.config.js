9ae3da2a0e7b43c286d493a7ce007c44290215a7
Justin <ImmaZoni@gmail.com>
2022-03-03 16:28:44 -0700

    Add: Sitemap.xml

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,6 +29,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        sitemap: {
+          changefreq: 'weekly',
+          priority: 0.5,
+        },
       }),
     ],
   ],
