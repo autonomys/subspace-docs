1564c541dcb6595c877d2358946a8f0a282afa4a
extSunset <103024509+extSunset@users.noreply.github.com>
2023-08-21 21:09:31 +0300

    Improve the docker-compose generator interface and shorten the page generation template.

--- src/components/DockerFileGenerator/index.jsx
+++ src/components/DockerFileGenerator/index.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import CodeBlock from '@theme/CodeBlock'
 
+import styles from './DockerFileGenerator.module.css';
+
 // This component is a Docker Compose File Generator for Subspace Docs.
 function DockerFileGenerator() {
     // Initial state for form data
@@ -68,7 +70,7 @@ function DockerFileGenerator() {
     }, []);
 
 
-     // Generates the Docker Compose file content based on formData
+    // Generates the Docker Compose file content based on formData
     const generateOutput = () => {
         const validationErrors = validateInputs();
 
@@ -182,175 +184,70 @@ node:
     };
 
     return (
-        <div className="container margin-vert--lg">
-	    <h2 className="text--center margin-bottom--m">Docker Compose File Generator</h2>
+        <div className={`container ${styles['container--form']} margin-vert--lg`}>
+            <h2 className="text--center margin-bottom--m">Docker Compose File Generator</h2>
 
-	    <div className="card">
+            <div className={`card ${styles['beautiful-form']}`} >
                 <div className="card__body">
+                    
+                    {[
+			{ label: 'Node Port', name: 'nodePort' },
+			{ label: 'Node DSN Port', name: 'nodeDsnPort' },
+			{ label: 'Farmer Port', name: 'farmerPort' },
+			{ label: 'Node Name', name: 'nodeName' },
+			{ label: 'Plot Size', name: 'plotSize' },
+			{ label: 'Reward Address', name: 'rewardAddress' },
+			{ label: 'Node Data (Optional)', name: 'nodeData' },
+			{ label: 'Farmer Data (Optional)', name: 'farmerData' }
+                    ].map(({ label, name }) => (
+			<div className={styles['form-group']}>
+                            <label htmlFor={name} className="form-label text--bold">{label}:</label>
+                            <input
+				className={styles['form-input']}
+				name={name}
+				placeholder={label}
+				onChange={handleChange}
+				value={formData[name]}
+                            />
+                            {errors[name] && <p className={styles['text--error']}>{errors[name]}</p>}
+			</div>
+                    ))}
+
+		    <div className={styles['form-group']}>
+			<label htmlFor="snapshot" className="form-label text--bold">Snapshot:</label>
+			<select
+			    className={styles['form-input']}
+			    name="snapshot"
+			    value={formData.snapshot}
+			    onChange={handleChange}
+			>
+		            {snapshots.map(snap => <option key={snap} value={snap}>{snap}</option>)}
+			</select>
+		    </div>
+
+		    <div className={styles['form-group']}>		    
+		        <label htmlFor="arch" className="form-label text--bold">Architecture:</label>
+			<select
+                            className={styles['form-input']}
+                            name="arch"
+                            value={formData.arch}
+                            onChange={handleChange}
+			>
+                            <option value="x86_64">x86_64 (skylake+)</option>
+                            <option value="aarch64">aarch64</option>
+			</select>
+		    </div>
 		    
-		    <div className="form-group row margin-bottom--sm">
-			<label htmlFor="nodePort" className="col col--4 form-label text--bold">Node Port:</label>
-			<div className="col col--8" style={{ paddingLeft: '1rem', paddingRight: '1rem' }}>
-			    <input
-				className="form-input"
-				style={{ width: '100%', padding: '1rem', fontSize: '1.1rem' }}
-				name="nodePort" 
-				placeholder="Node Port" 
-				onChange={handleChange} 
-				value={formData.nodePort}
-			    />
-			    {errors.nodePort && <p style={{ color: 'red' }} className="text--error">{errors.nodePort}</p>}
-			</div>
-		    </div>
-
-		    <div className="form-group row margin-bottom--sm">
-			<label htmlFor="nodeDsnPort" className="col col--4 form-label text--bold">Node DSN Port:</label>
-			<div className="col col--8" style={{ paddingLeft: '1rem', paddingRight: '1rem' }}> 
-			    <input
-				className="form-input"
-				style={{ width: '100%', padding: '1rem', fontSize: '1.1rem' }}
-				name="nodeDsnPort" 
-				placeholder="Node DSN Port" 
-				onChange={handleChange} 
-				value={formData.nodeDsnPort}
-			    />
-			    {errors.nodeDsnPort && <p style={{ color: 'red' }} className="text--error">{errors.nodeDsnPort}</p>}
-			</div>
-		    </div>
-
-		    <div className="form-group row margin-bottom--sm">
-			<label htmlFor="farmerPort" className="col col--4 form-label text--bold">Farmer Port:</label>
-			<div className="col col--8" style={{ paddingLeft: '1rem', paddingRight: '1rem' }}>
-			    <input
-				className="form-input"
-				style={{ width: '100%', padding: '1rem', fontSize: '1.1rem' }}
-				name="farmerPort" 
-				placeholder="Farmer Port" 
-				onChange={handleChange} 
-				value={formData.farmerPort}
-			    />
-			    {errors.farmerPort && <p style={{ color: 'red' }} className="text--error">{errors.farmerPort}</p>}
-			</div>
-		    </div>
-
-		    <div className="form-group row margin-bottom--sm">
-			<label htmlFor="nodeName" className="col col--4 form-label text--bold">Node Name:</label>
-			<div className="col col--8" style={{ paddingLeft: '1rem', paddingRight: '1rem' }}>
-			    <input
-				className="form-input"
-				style={{ width: '100%', padding: '1rem', fontSize: '1.1rem' }}
-				name="nodeName" 
-				placeholder="Node Name" 
-				onChange={handleChange} 
-				value={formData.nodeName}
-			    />
-			    {errors.nodeName && <p style={{ color: 'red' }} className="text--error">{errors.nodeName}</p>}
-			</div>
-		    </div>
-
-		    <div className="form-group row margin-bottom--sm">
-			<label htmlFor="snapshot" className="col col--4 form-label text--bold">Snapshot:</label>
-			<div className="col col--8" style={{ paddingLeft: '1rem', paddingRight: '1rem' }}>
-			    <select
-				className="form-input"
-				style={{ width: '100%', padding: '1rem', fontSize: '1.1rem' }}
-				name="snapshot"
-				value={formData.snapshot}
-				onChange={handleChange}
-			    >
-				{snapshots.map(snap => <option key={snap} value={snap}>{snap}</option>)}
-			    </select>
-			</div>
-		    </div>
-
-		    <div className="form-group row margin-bottom--sm">
-			<label htmlFor="arch" className="col col--4 form-label text--bold">Architecture:</label>
-			<div className="col col--8" style={{ paddingLeft: '1rem', paddingRight: '1rem' }}>
-			    <select
-				className="form-input"
-				style={{ width: '100%', padding: '1rem', fontSize: '1.1rem' }}
-				name="arch"
-				value={formData.arch}
-				onChange={handleChange}
-			    >
-				<option value="x86_64">x86_64 (skylake+)</option>
-				<option value="aarch64">aarch64</option>
-			    </select>
-			</div>
-		    </div>
-
-
-		    <div className="form-group row margin-bottom--sm">
-			<label htmlFor="plotSizet" className="col col--4 form-label text--bold">Plot Size:</label>
-			<div className="col col--8" style={{ paddingLeft: '1rem', paddingRight: '1rem' }}>
-			    <input
-				className="form-input"
-				style={{ width: '100%', padding: '1rem', fontSize: '1.1rem' }}
-				name="plotSize" 
-				placeholder="Plot Size" 
-				onChange={handleChange} 
-				value={formData.plotSize}
-			    />
-			    {errors.plotSize && <p style={{ color: 'red' }} className="text--error">{errors.plotSize}</p>}
-			</div>
-		    </div>
-
-		    <div className="form-group row margin-bottom--sm">
-			<label htmlFor="rewardAddress" className="col col--4 form-label text--bold">Reward Address:</label>
-			<div className="col col--8" style={{ paddingLeft: '1rem', paddingRight: '1rem' }}> 
-			    <input
-				className="form-input"
-				style={{ width: '100%', padding: '1rem', fontSize: '1.1rem' }}
-				name="rewardAddress" 
-				placeholder="Reward Address" 
-				onChange={handleChange} 
-				value={formData.rewardAddress}
-			    />
-			    {errors.rewardAddress && <p style={{ color: 'red' }} className="text--error">{errors.rewardAddress}</p>}
-			</div>
-		    </div>
-
-		    <div className="form-group row margin-bottom--sm">
-			<label htmlFor="nodeData" className="col col--4 form-label text--bold">Node Data (Optional):</label>
-			<div className="col col--8" style={{ paddingLeft: '1rem', paddingRight: '1rem' }}>
-			    <input
-				className="form-input"
-				style={{ width: '100%', padding: '1rem', fontSize: '1.1rem' }}
-				name="nodeData" 
-				placeholder="Node Data (Optional)" 
-				onChange={handleChange} 
-				value={formData.nodeData}
-			    />
-			    {errors.nodeData && <p style={{ color: 'red' }} className="text--error">{errors.nodeData}</p>}
-			</div>
-		    </div>
-
-		    <div className="form-group row margin-bottom--sm">
-			<label htmlFor="farmerData" className="col col--4 form-label text--bold">Farmer Data (Optional):</label>
-			<div className="col col--8" style={{ paddingLeft: '1rem', paddingRight: '1rem' }}> 
-			    <input
-				className="form-input"
-				style={{ width: '100%', padding: '1rem', fontSize: '1.1rem' }}
-				name="farmerData" 
-				placeholder="Farmer Data (Optional)" 
-				onChange={handleChange} 
-				value={formData.farmerData}
-			    />
-			    {errors.farmerData && <p style={{ color: 'red' }} className="text--error">{errors.farmerData}</p>}
-			</div>
-		    </div>
-
-		    <div className="form-group row margin-top--md">
-                        <div className="col col--12 text--center">
-                            <button className="button button--primary button--lg" onClick={generateOutput}>Generate</button>
-                        </div>
+                    
+                    <div className={`${styles['form-group']} text--center`}>
+                        <button className={`button button--primary button--lg ${styles['button--primary-emphasized']}`} onClick={generateOutput}>Generate</button>
                     </div>
-		</div>
-	    </div>
-	    
-	    <div className="margin-top--lg">
-		<CodeBlock language="yaml" title="docker-compose.yaml">{output}</CodeBlock>
-	    </div>
+                </div>
+            </div>
+            
+            <div className="margin-top--lg">
+                <CodeBlock language="yaml" title="docker-compose.yaml">{output}</CodeBlock>
+            </div>
         </div>
     );
 }
