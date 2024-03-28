ef22756b1285af308f58b44ae584fb00a9b429ca
Emil Fattakhov <emil.e.fattakhov@gmail.com>
2024-03-28 09:26:36 -0400

    Hide search bar until docs are crawled through

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -302,10 +302,6 @@ const config = {
             href: 'https://github.com/subspace',
             label: 'GitHub',
             position: 'right',
-          },
-          {
-            type: 'search',
-            position: 'right',
           }
         ],
       },
@@ -388,33 +384,6 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
         additionalLanguages: ['toml', 'powershell', 'shell-session', 'ini'],
-      },
-      algolia: {
-            // The application ID provided by Algolia
-            appId: 'PURWQGSNUR',
-
-            // Public API key: it is safe to commit it
-            apiKey: '3317f8373bc49a240f45964de434250c',
-
-            indexName: 'subspace-documentation',
-
-            // Optional: see doc section below
-            contextualSearch: true,
-
-            // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-            //externalUrlRegex: 'external\\.com|domain\\.com',
-
-            // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-            //replaceSearchResultPathname: {
-            //  from: '/docs/', // or as RegExp: /\/docs\//
-            //  to: '/',
-            //},
-
-            // Optional: Algolia search parameters
-            searchParameters: {},
-
-            // Optional: path for search page that enabled by default (`false` to disable it)
-            searchPagePath: false,
       }
     }),
 };
