3e30715c3eb51ea6fb23f01326fefe13a3eb658e
vexr <github@mail.qco.io>
2024-10-13 00:22:08 -0700

    Move button component to src/components

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // Importing the original mapper + our components according to the Docusaurus doc
 import MDXComponents from '@theme-original/MDXComponents';
-import Button from '@site/src/theme/DocButton';
+import Button from '@site/src/components/Button';
 export default {
   // Reusing the default mapping
   ...MDXComponents,
