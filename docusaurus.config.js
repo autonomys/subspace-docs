5f3cbf9bfb68b79b8f4cf8ad1d2b204866dc349c
Emil Fattakhov <emil.e.fattakhov@gmail.com>
2024-11-19 11:01:57 -0500

    Remove arabic and polish as there're no contributions

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
   // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale,
-    locales: ["ar", "de", "en", "fr", "id", "it", "ja", "uk", "zh", "pl", "pt", "ru", "vi", "es", "tr"],
+    locales: ["de", "en", "fr", "id", "it", "ja", "uk", "zh", "pt", "ru", "vi", "es", "tr"],
   },
   
   markdown: {
